106f79a600affd50c9a023765b2165d71796bf41
v1nnyc <cannallav019@yahoo.com>
2018-01-10 22:01:02 -0800

    implemented heap sort and added a switch for choices

--- sorting.cpp
+++ sorting.cpp
@@ -46,6 +46,7 @@ static void insertionSort(){
         j = j -1;
     }
     *(arrayUsed + (j+1)) = x;
+    printArray();
     }
 }
 
@@ -58,19 +59,92 @@ static void bubbleSort(){
                 *(arrayUsed + j) = l;
                 *(arrayUsed + (j+1)) = k;
             }
+            printArray();
         }
+    }
+}
+
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. n is size of heap
+void heapify(int n, int i)
+{
+    int largest = i;  // Initialize largest as root
+    int l = 2*i + 1;  // left = 2*i + 1
+    int r = 2*i + 2;  // right = 2*i + 2
+ 
+    // If left child is larger than root
+    if (l < n && *(arrayUsed + l) > *(arrayUsed + largest))
+        largest = l;
+ 
+    // If right child is larger than largest so far
+    if (r < n && *(arrayUsed + r) > *(arrayUsed + largest))
+        largest = r;
+ 
+    // If largest is not root
+    if (largest != i)
+    {
+        int k = *(arrayUsed + i);
+        int l = *(arrayUsed + largest);
+        *(arrayUsed + i) = l;
+        *(arrayUsed + largest) = k;
+
+ 
+        // Recursively heapify the affected sub-tree
+        heapify(n, largest);
+    }
+}
+ 
+// main function to do heap sort
+static void heapSort(int n)
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(n, i);
+ 
+    // One by one extract an element from heap
+    for (int i=n-1; i>=0; i--)
+    {
+        // Move current root to end
+        swap(*arrayUsed, *(arrayUsed + i));
+ 
+        // call max heapify on the reduced heap
+        heapify( i, 0);
+        printArray();
     }
 }
 
 int main(){
     decideSize();
+
+    
+    cout << "cool, now pick a sorting algorithm \n";
+    
+    int input;
+    
+    cout<<" 1. insertion sort\n";
+    cout<<" 2. bubble sort\n ";
+    cout<<" 3. heap sort\n ";
+    cout<<"Selection: ";
+   
+    cin>> input;
     fillArray();
     printArray();
-    bubbleSort();
-    printArray();
-    fillArray();
-    printArray();
+
+    switch ( input ) {
+case 1:            // Note the colon, not a semicolon
     insertionSort();
+    break;
+case 2:
+    bubbleSort();
+    break;
+case 3: 
+    heapSort(arraySize);
+    break;
+default:            // Note the colon, not a semicolon
+    cout<<"Error, bad input, quitting\n";
+    break;
+  }
     printArray();
+
     delete[] arrayUsed;
 }
